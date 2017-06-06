56ed58b0dbaced20374883b75e8495f061effd35
Tri S <mas.trisutrisno@gmail.com>
2017-06-06 16:10:01 +0700

    add bootsrap style

--- application/views/admin/v_setting.php
+++ application/views/admin/v_setting.php
@@ -3,41 +3,50 @@
  ?><!DOCTYPE html>  
  <head>
    <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
    <title>
-     Dashboard 
+     Setting 
    </title>
+   <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
  </head>
  <body>
-      <h2>Setting</h2>
-<form action="<?php echo base_url()."beranda/admin"?>" method="post">	 
-    
-    
-    <p>Buka Pairing</p>
-    
-    Status : <?php 
-    
-    if($data==0){
-        echo "Close";
-    }else{
-        echo "Open";
-    }
-        
-    ?>
-    
-    <br>
-    
-    <select name="status">
+     <div class="jumbotron text-center">
+        <h1>Setting Page</h1>
+        <p>to open and close pairing system</p> 
+        </div>
+     <div class="container-fluid">
+      
+     
+     <div class="row">
+         <div class="col-xs-6">
+     
+        <div class="well well-sm">Status : <?php 
+        if($data==0){
+                echo "Close";
+            }else{
+                echo "Open";
+            }?>
+        </div>
+
+     <form action="<?php echo base_url()."beranda/admin"?>" method="post">	 
+    <div class="form-group">
+        <select name="status" class="form-control">
         <option value="1">Open</option>
         <option value="0">Close</option>
     </select>
-    
+    </div>
     <p>
-     <input type="submit" name="btnSubmit" value="Submit" />
+     <input type="submit" name="btnSubmit" value="Submit" class="btn btn-default" />
      </p>
  
 </form> 
+         </div>
+         </div>
      <p>
      Kembali ke dashboard, Silakan klik <?php echo anchor(site_url().'dashboard','di sini..'); ?>
      </p>
+     
+     </div>
  </body>
  </html>

591149439579d57b16c05da87079899f9e1ab563
ismiad1202 <ismiadi.andriawan@gmail.com>
2017-06-06 14:47:14 +0700

    Rules Kado
    update kata aja

--- application/views/rules.php
+++ application/views/rules.php
@@ -5,13 +5,13 @@
 <li>Registrasi &amp; Pengisian Form Kado (Deadline 5-7 Juni)</li>
 <li>Peraturan&nbsp;Pengisian Kado
 <ol>
-<li>Limit masing-masing kado adalah <strong>Rp. 100.000</strong>, <strong>maksimal harga kado Rp. 150.000</strong> (max dana talangan Rp. 25.000)</li>
+<li>Limit masing-masing kado adalah <strong>Rp. 100.000</strong>, <strong>maksimal harga kado Rp. 150.000</strong> (max dana talangan Rp. 50.000)</li>
 <li>Tolong usahakan mengisi barang yang sesuai dengan harga limit / harga maksimal</li>
 <li>3 kado, diisi dengan barang berbeda&nbsp;<strong>masing-masing</strong> harganya sesuai limit, jadi nanti pemberi akan <strong>memilih salah satu</strong> dari 3 kado, dan itu akan menjadi milikmu. Kejutuan selalu menyenangkan :)</li>
 <li>Kado masih <strong>bisa di edit</strong>, sampai tanggal deadline (7 juni), setelah itu, akan dikunci dan tidak dapat diubah.</li>
-<li><strong>Terms and conditions</strong>: Mohon pilih <strong>yes</strong>, jika <strong>bersedia</strong> menerima kado dengan harga melebihi limit (maksimal harga kado Rp. 125.000, jadi kelebihannya max Rp. 25.000)</li>
+<li><strong>Terms and conditions</strong>: Mohon pilih <strong>yes</strong>, jika <strong>bersedia</strong> menerima kado dengan harga melebihi limit (maksimal harga kado Rp. 150.000, jadi kelebihannya max Rp. 50.000)</li>
 <li><strong>Terms and conditions</strong>: Mohon pilih <strong>no,</strong> jika <strong>tidak</strong> <strong>bersedia</strong> menerima kado diatas limit.</li>
-<li>Pemberi <strong>JANGAN</strong>&nbsp;memberi kado lebih dari limit, <strong>KECUALI</strong> si penerima menyatakan bersedia membayar kelebihan (poin 4)</li>
+<li>Pemberi <strong>JANGAN</strong>&nbsp;memberi kado lebih dari limit, <strong>KECUALI</strong> si penerima menyatakan bersedia membayar kelebihan (poin 5)</li>
 <li>Jika harga barang kurang dari harga limit, maka selisihnya dimasukkan kedalam kado (contoh harga barang Rp. 80.000, maka Rp. 20.000 dimasukkan ke kado)</li>
 <li>Bon pembelian kado, harus dimasukkan kedalam kado, Tanpa nama pemberi</li>
 <li>Kado dibungkus dengan warna putih polos (bagian belakang kertas kalender)</li>
